8de3a7e51771583fd02ec9a05c438ad824a27857
Augustus Seabrooke <august.seabrooke@gmail.com>
2026-09-23 16:08:27 -0700

    Make AminoBar a single menu-bar app entry point

--- AminoBar/AminoBar/AminoBarApp.swift
+++ AminoBar/AminoBar/AminoBarApp.swift
@@ -1,17 +1,34 @@
-//
-//  AminoBarApp.swift
-//  AminoBar
-//
-//  Created by Augustus Seabrooke on 9/7/25.
-//
-
 import SwiftUI
 
 @main
 struct AminoBarApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra("AminoBar", systemImage: "testtube.2") {
+            AminoPanel()
+                .frame(width: 380)
+                .padding(.vertical, 8)
+
+            Divider()
+
+            SettingsLink {
+                Text("About AminoBar…")
+            }
+
+            Button("Quit AminoBar") {
+                NSApplication.shared.terminate(nil)
+            }
+        }
+        .menuBarExtraStyle(.window)
+
+        Settings {
+            AboutView()
+                .frame(width: 420, height: 260)
+        }
+        .commands {
+            CommandGroup(replacing: .find) {
+                Button("Focus Search", action: FocusSearchCenter.shared.focusSearch)
+                    .keyboardShortcut("f", modifiers: [.command])
+            }
         }
     }
 }
